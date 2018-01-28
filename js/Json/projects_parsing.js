ef08fff5adf1a190f9383ae1c41fe68fc14dcafd
DESKTOP-CN8NUDP\RIM <ghl2682@gmail.com>
2018-01-28 18:27:57 -0500

    Modified projects part

--- js/Json/projects_parsing.js
+++ js/Json/projects_parsing.js
@@ -2,8 +2,8 @@ function parsingProjectsForIndex() {
     var htm = "";
     $.each($.parseJSON(projects), function () {
         htm += "<div class='col-lg-12'><div class='thumbnail'>" +
-            "<img src=" + this.imgurl + " alt ='projects image' >" +
-            "</div> <div class='caption'><h3>" +this.caption+"</h3><p>Some sample text. Some sample text.</p></div></div>";
+            "<img src=" + this.imgurl + " alt ='projects image' height = "+this.height+"width="+this.width+" >" +
+            "</div> <div class='caption'><h3>" +this.caption+"</h3><p>"+this.description+"</p></div></div><br><br><br>";
     });
     document.getElementById("rowProjectsDiv").innerHTML = htm;
 }
