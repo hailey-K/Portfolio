cf746b6c412d2b010eda0346dec0955f944e3ca6
DESKTOP-CN8NUDP\RIM <ghl2682@gmail.com>
2018-01-30 12:22:26 -0500

    Modified Projects part

--- js/Json/projects_parsing.js
+++ js/Json/projects_parsing.js
@@ -1,9 +1,23 @@
 function parsingProjectsForIndex() {
     var htm = "";
     $.each($.parseJSON(projects), function () {
-        htm += "<div class='col-lg-12'><div class='thumbnail'>" +
-            "<img src=" + this.imgurl + " alt ='projects image' height = "+this.height+"width="+this.width+" >" +
-            "</div> <div class='caption'><h3>" +this.caption+"</h3><p>"+this.description+"</p></div></div><br><br><br>";
-    });
+  /*    htm += "<div class='col-lg-12'><div class='thumbnail' id='imageSection'>" +
+            "<img src=" + this.imgurl + " alt ='projects image' height ="+this.height+"width="+this.width+">" +
+            "</div> <div class='caption' id='imageSection'><h3>" +this.caption+"</h3><p>"+this.description+"</p></div></div><br><br><br>";*/
+    
+    /*        htm += "<div id='imageCaptionDiv'><div id='projectImageDiv'>" +
+            "<img src=" + this.imgurl + " alt ='projects image' height ="+this.height+"width="+this.width+">" +
+            "</div> <div id='projectCaptionDiv'><h4>" +this.caption+"</h4><p>"+this.description+"</p></div></div>";
+*/
+   htm += "<div class='row' id='imageCaptionDiv'><div class='col-lg-6'>" +
+            "<img src=" + this.imgurl + " alt ='projects image' height ="+this.height+"width="+this.width+">" +
+            "</div> <div class='col-lg-6'><h4>" +this.caption+"</h4><p>"+this.description+"</p></div></div>";
+
+            if(this.imgurl != "js/json/img/4.png")
+                {
+ htm += "<hr>";
+                }
+        });
+            
     document.getElementById("rowProjectsDiv").innerHTML = htm;
 }
